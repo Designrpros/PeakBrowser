12ee5f5582a4088cbca35827170723ae4407bdba
Vegar Berentsen <VegarBerentsen@gmail.com>
2026-07-03 15:51:38 +0200

    fix next config merge conflict

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   compiler: {
     styledComponents: true,
   },
-=======
-  /* config options here */
->>>>>>> 0ad4fa7d52b391ce7555aa0011289117ffed3869
 };
 
 export default nextConfig;
